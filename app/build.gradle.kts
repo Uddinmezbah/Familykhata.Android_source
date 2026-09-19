17849383ff2db7606d7e9f1cac4402c533351745
Uddinmezbah <uddinmezbah13@gmail.com>
2026-09-19 17:10:08 +0600

    Prepare Hisabi Khata 1.6.0 release identity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 16
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
